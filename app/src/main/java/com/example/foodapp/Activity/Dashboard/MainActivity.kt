75d9dce0ff886c23e326abc598135b0bd8e98c9f
Alonza <a.lozova.s@gmail.com>
2025-10-18 14:41:53 +0300

    Top bar

--- app/src/main/java/com/example/foodapp/Activity/Dashboard/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/Activity/Dashboard/MainActivity.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.foodapp.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            MainScreen()
         }
     }
 }
@@ -28,6 +40,23 @@ fun MainScreen() {
         },
         scaffoldState = scaffoldState
     ) { paddingValues ->
-
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .fillMaxSize()
+                .background(colorResource(R.color.lightGrey))
+                .padding(paddingValues),
+            contentPadding = PaddingValues(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            item(
+                span = {
+                    GridItemSpan(2)
+                }
+            ) {
+                TopBar()
+            }
+        }
     }
 }
