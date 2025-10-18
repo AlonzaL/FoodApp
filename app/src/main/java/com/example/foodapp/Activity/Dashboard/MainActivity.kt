8eaab1d871d0509494e140f8ca483345819e856c
Alonza <a.lozova.s@gmail.com>
2025-10-18 15:21:03 +0300

    Category List

--- app/src/main/java/com/example/foodapp/Activity/Dashboard/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/Activity/Dashboard/MainActivity.kt
@@ -15,10 +15,18 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
+import com.example.foodapp.Domain.CategoryModel
 import com.example.foodapp.R
+import com.example.foodapp.ViewModel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +41,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val scaffoldState = rememberScaffoldState()
+    val viewModel = MainViewModel()
+
+    val categories = remember { mutableStateListOf<CategoryModel>() }
+
+    var showCategoryLoading by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        viewModel.loadCategory().observeForever {
+            categories.clear()
+            categories.addAll(it)
+            showCategoryLoading = false
+        }
+    }
 
     Scaffold(
         bottomBar = {
@@ -56,6 +77,14 @@ fun MainScreen() {
                 }
             ) {
                 TopBar()
+            }
+            item(
+                span = { GridItemSpan(2) }
+            ) {
+                CategorySection(
+                    categories = categories,
+                    showCategoryLoading = showCategoryLoading
+                )
             }
         }
     }
